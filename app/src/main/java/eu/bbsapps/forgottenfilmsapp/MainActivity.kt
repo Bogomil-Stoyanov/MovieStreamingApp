79f713ccb3da1f7f271bca36f6f6d5b98ae7b88d
Bogomil Stoyanov <68944549+Bogomil-Stoyanov@users.noreply.github.com>
2022-01-31 18:07:01 +0200

    Initial commit. Package Structure

---
+++ app/src/main/java/eu/bbsapps/forgottenfilmsapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package eu.bbsapps.forgottenfilmsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import eu.bbsapps.forgottenfilmsapp.presentation.ui.theme.ForgottenFilmsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ForgottenFilmsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ForgottenFilmsAppTheme {
+        Greeting("Android")
+    }
+}
